62b8167def8f3fc4955c4f45f36eec8fd42628c7
Rachel Knight <104657939+orangecat268@users.noreply.github.com>
2022-05-24 13:22:14 -0400

    Update display_jumper_image.py

---
+++ game/display_jumper_image.py
@@ -0,0 +1,63 @@
+
+class display_jumper_image:
+    # Displays each image of jumper starting from
+    # no misses to run out of turns
+    def player_guess(guess):
+        if guess == 1: 
+            print("_ _ _ _ _")
+            print(" ")
+            print("  ___")
+            print(" /___\\")
+            print(" \   / ")
+            print("  \ / ")
+            print("   O   ")
+            print("  /|\\ ")
+            print("  / \ ")
+            print(" ")
+            print("^^^^^^^")
+        elif guess == 2:
+            print("_ _ _ _ _")
+            print(" ")
+            print(" /___\\")
+            print(" \   / ")
+            print("  \ / ")
+            print("   O   ")
+            print("  /|\\ ")
+            print("  / \ ")
+            print(" ")
+            print("^^^^^^^")
+        elif guess == 3:
+            print("_ _ _ _ _")
+            print(" ")
+            print(" \   / ")
+            print("  \ / ")
+            print("   O   ")
+            print("  /|\\ ")
+            print("  / \ ")
+            print(" ")
+            print("^^^^^^^")
+        elif guess == 4:
+            print("_ _ _ _ _")
+            print(" ")
+            print(" ")
+            print(" ")
+            print(" ")
+            print("  \ / ")
+            print("   O   ")
+            print("  /|\\ ")
+            print("  / \ ")
+            print(" ")
+            print("^^^^^^^")
+        elif guess == 5:
+            print("_ _ _ _ _")
+            print(" ")
+            print("   x   ")
+            print("  /|\\ ")
+            print("  / \ ")
+            print(" ")
+            print("^^^^^^^")
+
+
+
+
+
