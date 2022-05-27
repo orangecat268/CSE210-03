154eb2eeb0033e92900c9164ddc63c519aba9a57
awsmudaliar <awsmudaliar@outlook.com>
2022-05-26 21:43:47 -0600

    Hidden Word
    Update

--- game/display_jumper_image.py
+++ game/display_jumper_image.py
@@ -58,17 +58,7 @@ class display_jumper_image:
             print(" ")
             print("^^^^^^^")
         else:
-            print("_ _ _ _ _")
-            print(" ")
-            print("  ___")
-            print(" /___\\")
-            print(" \   / ")
-            print("  \ / ")
-            print("   O   ")
-            print("  /|\\ ")
-            print("  / \ ")
-            print(" ")
-            print("^^^^^^^")
+            print("")
 
     def play_screen_update(random_word, incorrect_guesses, hidden_word, eval_guess=0, LIMIT=5, game_status="2", last_tried=""):
 
@@ -85,17 +75,13 @@ class display_jumper_image:
             print(f"Game Over - the word was: {random_word} \n")
             print(f"Incorrect guesses: {incorrect_guesses} \n")
         elif game_status == 1:
-            print(f"WOOOOOHOOOOOOOO")
+            print(f"WOOOOOHOOOOOOOO \n")
             print(f"YOU WIN - the word was: {random_word} \n")
             print(f"Incorrect guesses: {incorrect_guesses} \n")
         else:
-            print(f"\nYour hidden word looks like this: {hidden_word}")
-            if len(incorrect_guesses) > 0:
-                print(
-                    f"Incorrect guesses: '{incorrect_guesses}'")
-            elif len(incorrect_guesses) > 1:
-                print(
-                    f"Incorrect guesses: '{incorrect_guesses}'")
+            print(f"\nHidden word: {hidden_word}")
+            if len(incorrect_guesses) > 0 or len(incorrect_guesses) > 1:
+                print(f"Incorrect guesses: '{incorrect_guesses}'")
 
             guesses_left = 0
             guesses_left = 5 - int(len(incorrect_guesses))
